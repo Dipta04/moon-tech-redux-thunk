45b855a88f5613326ad5a34e028c155aee71c582
Dipta04 <diptakarmakar38@gmail.com>
2023-02-07 21:23:03 +0600

    first commit

---
+++ src/routes/routes.js
@@ -0,0 +1,49 @@
+import { createBrowserRouter } from "react-router-dom";
+import Dashboard from "../layout/Dashboard/Dashboard";
+import Main from "../layout/Main/Main";
+import AddProduct from "../pages/Dashboard/AddProduct";
+import ProductList from "../pages/Dashboard/ProductList";
+import About from "../pages/Main/About";
+import Cart from "../pages/Main/Cart";
+import Home from "../pages/Main/Home";
+import TopRated from "../pages/Main/TopRated";
+
+const routes = createBrowserRouter([
+    {
+        path: "/",
+        element: <Main></Main>,
+        children: [
+            {
+                path: '/',
+                element: <Home></Home>
+            },
+            {
+                path: 'about',
+                element: <About></About>
+            },
+            {
+                path: 'top-rated',
+                element: <TopRated></TopRated>
+            },
+            {
+                path: 'cart',
+                element: <Cart></Cart>
+            }
+        ]
+    },
+    {
+        path: "/dashboard",
+        element: <Dashboard></Dashboard>,
+        children: [
+            {
+                path: "/dashboard",
+                element: <ProductList></ProductList>,
+            },
+            {
+                path: "add-product",
+                element: <AddProduct></AddProduct>,
+            }
+        ]
+    }
+])
+export default routes;
